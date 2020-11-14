aee63242ef4f344904e6476a4ba7bae710fe9eb6
NilsonCapetillo <62961588+NilsonCapetillo@users.noreply.github.com>
2020-11-13 22:43:15 -0600

    interfaz de usuario

---
+++ app/src/main/java/com/example/autoreserv/Modificar_Cuenta.java
@@ -0,0 +1,14 @@
+package com.example.autoreserv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Modificar_Cuenta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modificar__cuenta);
+    }
+}
