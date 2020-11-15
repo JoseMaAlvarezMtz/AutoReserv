56c86f2c386a7368a5c61a88d79328991754d728
NilsonCapetillo <62961588+NilsonCapetillo@users.noreply.github.com>
2020-11-15 16:36:25 -0600

    interfaz de usuario

---
+++ app/src/main/java/com/example/autoreserv/Expandir.java
@@ -0,0 +1,14 @@
+package com.example.autoreserv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Expandir extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expandir);
+    }
+}
